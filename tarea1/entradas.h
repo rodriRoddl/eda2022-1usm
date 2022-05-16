904e284e8ea6398acff095cdd4daba523897ff60
rodriRoddl <rodrigo.lobosc@usm.cl>
2022-05-16 11:28:47 -0400

    agreagado values y txt de ejemplo

---
+++ tarea1/entradas.h
@@ -0,0 +1,23 @@
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+const double pct_interno = 0.25;
+const int cant_externo = 5000;
+typedef struct seccion {
+    int id ;
+    int asientos ;
+    float factor ;
+    struct seccion *sgte;
+} sector ;
+typedef struct estadio {
+    int cant_sec ;
+    int pbase ;
+    sector * secc ;
+} stadium ;
+typedef struct cliente {
+    char * id ;
+    int presupuesto ;
+    int n_seccion ;
+    int n_entradas ;
+    struct cliente * sgte ;
+} fans ;
---
+++ tarea1/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "entradas.h"
+
+int main(int argc, char **argv){
+    
+
+
+    return 0;
+}
