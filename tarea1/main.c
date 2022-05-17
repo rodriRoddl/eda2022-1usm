cae95772dc96912f67247ba15cbe773ae734dd54
rodriRoddl <rodrigo.lobosc@usm.cl>
2022-05-17 01:41:34 -0400

    configurado para prueba

--- tarea1/main.c
+++ tarea1/main.c
@@ -4,7 +4,11 @@
 
 int main(int argc, char **argv){
     FILE *arch;
-    arch = fopen(argv[0],"r");
+    arch = fopen(argv[1],"r");
+    char nro[20],se[20];
+    int nsec,pre;
+    fscanf(arch,"%s%d%s%d",nro,&nsec,se,&pre);
+    fprintf(stdout,"%s %d %s %d ",nro,nsec,se,pre);
     fclose(arch);
     return 0;
 }
