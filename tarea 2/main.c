773cbf883f2cc31719c04e03e65e7d3c62dbd06e
rodriRoddl <rodrigo.lobosc@usm.cl>
2022-07-10 21:40:45 -0400

    Agregado algoritmos (NO TERMINADO Y ERROR)

--- tarea 2/main.c
+++ tarea 2/main.c
@@ -2,34 +2,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
 int main(int argc, char *argv[]){
     FILE *arch;
     arch = fopen(argv[1],"r");
     int it = 0, append = 0;
-    char c;
-    for (c = getc(arch); c != EOF; c = getc(arch)){
-        if (c == '\n'){
-            it = it + 1;
-        }
+    char c[2100];
+    while(fscanf(arch, "%[^\n] ", c) != EOF){
+        it = it +1;
     }
-    tweet data[it];
+    tweet *data = malloc(it*sizeof(tweet));
     rewind(arch);
     while(!feof(arch)){
-        data[append].fecha = malloc(sizeof(char*)*12);
-        data[append].hora = malloc(sizeof(char*)*12);
-        data[append].user = malloc(sizeof(char*)*12);
-        data[append].mensaje = malloc(sizeof(char*)*355);
+        data[append].fecha = malloc(sizeof(char)*20);
+        data[append].hora = malloc(sizeof(char)*20);
+        data[append].user = malloc(sizeof(char)*20);
+        data[append].mensaje = malloc(sizeof(char)*2000);
         fscanf(arch,"%[^;];%[^;];%[^;];%[^\n]",data[append].fecha,data[append].hora,data[append].user,data[append].mensaje);
-        //printf("%s  %s  %s  %s\n",data[append].fecha,data[append].hora,data[append].user,data[append].mensaje);
+        //printf("%d\n",append);
         append = append +1;
     }
-    printf("funciona\n");
-    insertionSort(data,it);
-    //uso de insert sort para pregunta 1 de orden= n^2
-    //uso de merge sort para pregunta 2, de orden = nlog(n)
-    //crear sorting.c para las implementaciones de los algoritmos
+    int opcion;
+    printf("elija el algoritmo de ordenamiento:");
+    printf("1)InsertSort (n^2)\n2)mergeSort(n log n)\n3)Radixsort(d(c*n))\n");
+    printf("elija el algoritmo de ordenamiento: ");
+    scanf("%d",&opcion);
+    if(opcion == 1){
+        insertionSort(data,it);
+    }
+    else if(opcion == 2){
+        mergeSort(data,0,it);
+    }
+    else if(opcion == 3){
+        RadixSort(data,it);
+    }
+    else{
+        printf("error al elegir metodo");
+    }
     fclose(arch);
     return 0;
 }
--- tarea 2/sorting.c
+++ tarea 2/sorting.c
@@ -2,16 +2,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-void insertionSort ( tweet arr [ ] , int n ){
+#include <time.h>
+#include <math.h>
+
+void insertionSort(tweet arr[], int n){
 int i, j;
+double value;
 tweet key;
-for ( i = 1 ; i < n ; i ++) {
-    key = arr [i] ;
-    j  = i - 1 ;
-    while ( j >= 0 && arr[j].fecha > key.fecha ) {
+clock_t start = clock();
+for (i= 1 ; i < n ; i ++){
+    key = arr[i];
+    j = i-1;
+    while(j >= 0 && (strcmp(arr[j].fecha,key.fecha) >= 0)){
         arr [j+1] = arr [j] ;
-        j --;
+        j--;
     }
-    arr [j + 1] = key ;
+    arr [j+1] = key;
+    }
+}
+
+void merge(tweet arr[], int l, int m,int r){
+    int i,j,k;
+    int n1 = m-l +1;
+    int n2 = r-m;
+    tweet L[n1], R[n2];
+    for(i = 0; i<n1; i++){
+        L[i] = arr[l+i];
+    }
+    for(j=0;j<n2;j++){
+        R[j] = arr[m+1+j];
+    }
+    i = 0; j = 0 ; k = l;
+    while(i<n1 && j<n2){
+        if(strcmp(L[i].fecha,R[j].fecha) <= 0){
+            arr[k] = L[i]; i++;
+        }
+        else{
+            arr[k] = R[j]; j++;
+        }
+        k++;
+    }
+    while(i<n1){
+        arr[k] = L[i]; i++;k++;
+    }
+    while( j < n2){
+        arr[k] = R[j]; j++;k++;
+    }
+}
+
+void mergeSort(tweet arr[],int l, int r){
+    if(l<r){
+    int m = l + (r-l)/2;
+    mergeSort(arr,l,m);
+    mergeSort(arr,m+1,r);
+    merge(arr,l,m,r);
+    }
+}
+
+tweet maximo(tweet *A, int tam){
+    int i;
+    tweet mayor = A[0];
+    for( i = 1; i< tam;i++){
+        if (strcmp(A[i].fecha,mayor.fecha) > 0){
+            mayor = A[i];
+        }
+    }
+    return mayor;
+}
+
+void CountingSort(tweet *A, int tam, int lugar){
+    tweet B[tam];
+    char y[4],m[2],d[2];
+    int  i , ind_sgte,veces,fecha;
+    int max = 0;
+    for(i=1;i<tam;i++){
+        sscanf(A[i].fecha,"%s:%s:%s",y,m,d);
+        strcat(y,m);
+        strcat(y,d);
+        fecha = atoi(y);
+        if(((fecha/lugar)%10)> max){
+            max = (fecha/lugar) %10;
+        }
+    }
+    //no se que mas hacer a partir de aca.
+}
+
+void RadixSort(tweet *A, int tam){
+    int pos, fecha;
+    char y[4],m[2],d[2];
+    tweet max = maximo(A,tam);
+    sscanf(max.fecha,"%s:%s:%s",y,m,d);
+    strcat(y,m);
+    strcat(y,d);
+    fecha = atoi(y);
+    printf("%d",fecha);
+    for(pos = 0; (fecha / pow(10,pos)) > 0 ;pos++){
+        CountingSort(A,tam,pow(10,pos));
     }
 }
